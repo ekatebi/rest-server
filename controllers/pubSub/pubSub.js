6bf73a4c10f94e8cebafa3e13713b9c907e6e867
Ed Katebi <ekatebi@gmail.com>
2017-11-24 21:03:03 -0500

    add publisher/subscriber

---
+++ controllers/pubSub/pubSub.js
@@ -0,0 +1,44 @@
+class PubSub {
+
+	static subscribers = {
+		// per topic
+		// topic: { msgs: [], listeners: [] }
+	};
+
+	static subscribe(topic, listener, msgsToReturn = 0) {
+
+		if (!PubSub.subscribers.topic) {
+			PubSub.subscribers[topic] = { msgs: [], listeners: [] };
+		}
+
+		const { msgs, listeners } = PubSub.subscribers[topic];
+
+		listeners.push(listener);
+		
+		if (msgsToReturn > 0) {
+			const startIndex = msgs.length > msgsToReturn ? msgs.length - msgsToReturn : 0;
+			for (let i = startIndex; i < msgs.length; i++) {
+				listener(msgs[i]);
+			}
+		}
+
+	}
+
+	static publish(topic, msg) {
+
+		if (!PubSub.subscribers.topic) {
+			PubSub.subscribers[topic] = { msgs: [], listeners: [] };
+		}
+
+		const { msgs, listeners } = PubSub.subscribers[topic];
+
+		msgs.push(msg);
+
+		listeners.forEach((listener, index) => {
+			listener(msg);
+		});
+	}
+
+};
+
+module.exports =  PubSub;
---
+++ controllers/pubSub/pubSub.test.js
@@ -0,0 +1,71 @@
+'use strict';
+
+import PubSub from './pubSub';
+
+describe('publisher/subscriber', () => {
+
+	const fooListener1 = (msg) => {
+		console.log('listener 1 for foo topic', msg);
+		expect(msg).toBe('hello');
+	};
+
+	const fooListener2 = (msg) => {
+		console.log('listener 2 for foo topic', msg);
+		expect(msg).toBe('hello');
+	};
+
+	const barListener1 = (msg) => {
+		console.log('listener 1 for bar topic', msg);
+		expect(msg).toBe('world');
+	};
+
+	const barListener2 = (msg) => {
+		console.log('listener 2 for bar topic', msg);
+		expect(msg).toBe('world');
+	};
+
+	const foo2Listener1 = (msg) => {
+		console.log('listener 1 for foo2 topic', msg);
+		expect(msg).toBe('hello');
+	};
+
+	const foo2Listener2 = (msg) => {
+		console.log('listener 2 for foo2 topic', msg);
+		expect(msg).toBe('hello 2');
+	};
+
+
+	test('subscribe', async () => {
+
+		PubSub.subscribe('foo', fooListener1);
+		PubSub.subscribe('foo', fooListener2);
+
+		PubSub.subscribe('bar', barListener1);
+		PubSub.subscribe('bar', barListener2);
+
+		PubSub.subscribe('foo2', foo2Listener1);
+		PubSub.subscribe('foo2', foo2Listener2);
+	});
+
+	test('publish', async () => {
+		PubSub.publish('foo', 'hello');
+		PubSub.publish('bar', 'world');
+		PubSub.publish('foo2', 'hello 2');
+	});
+
+	test('subscribe with history', async () => {
+
+		PubSub.subscribe('foo', fooListener1, 1);
+		PubSub.subscribe('bar', barListener1, 1);
+		PubSub.subscribe('foo2', foo2Listener1, 1);
+
+		PubSub.subscribe('foo', fooListener1, 2);
+		PubSub.subscribe('bar', barListener1, 2);
+		PubSub.subscribe('foo2', foo2Listener1, 2);
+
+		PubSub.subscribe('foo', fooListener1, 3);
+		PubSub.subscribe('bar', barListener1, 3);
+		PubSub.subscribe('foo2', foo2Listener1, 3);
+	});
+
+});
